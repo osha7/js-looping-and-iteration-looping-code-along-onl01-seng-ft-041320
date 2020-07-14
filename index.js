9582af0bb4cac974960be89de99681337de1df8d
osha7 <ogroetz@gmail.com>
2020-07-14 11:27:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++)
+// {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+// const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards( names, event ) {
+    let cards = []
+    for (let i = 0; i < names.length; i++)
+    {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+}
+
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
